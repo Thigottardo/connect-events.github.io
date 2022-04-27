f390b31d66ca6ec75098cce89e953cd9d3a80d87
Thigottardo <102668054+Thigottardo@users.noreply.github.com>
2022-04-27 16:32:39 -0300

    Add files via upload

--- assets/flutter_service_worker.js
+++ assets/flutter_service_worker.js
@@ -33,9 +33,9 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "7069612fa5b9c037a872afcae982f099",
-"/": "7069612fa5b9c037a872afcae982f099",
-"main.dart.js": "034408c618f5b0f7f00acebbfd7d207c",
+"index.html": "b2557e50ca3dbadaf3437c9efd90f121",
+"/": "b2557e50ca3dbadaf3437c9efd90f121",
+"main.dart.js": "c90328582ee617e93a6d0a97c93ec7c6",
 "manifest.json": "5289fc5e6b92084b8f2a43ad349e6114",
 "version.json": "146310228cddb209a88bfcd1205bb6a8"
 };
